0e27180ef4d03e7550386c9e3b8d4399d1cee13d
akumar16 <akumar16@c02r2bqjg8wp.fios-router.home>
2017-03-04 22:31:15 -0500

    random app project

---
+++ RandomApp2/app/src/main/java/com/example/akumar16/randomapp2/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.akumar16.randomapp2;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                //        .setAction("Action", null).show();
+                sendMessage(view);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void sendMessage(View view) {
+        //Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        EditText outputText = (EditText) findViewById(R.id.outputText);
+        String message = editText.getText().toString();
+        try {
+            int value = Integer.parseInt(message);
+            int result = value * 100;
+            Random rand = new Random();
+            result = rand.nextInt(value) + 1;
+            String resString = Integer.toString(result);
+            outputText.setText(resString);
+        }
+        catch (Exception e) {
+            outputText.setText("ERROR!");
+        }
+        //intent.putExtra(EXTRA_MESSAGE, message);
+        //startActivity(intent);
+    }
+}
